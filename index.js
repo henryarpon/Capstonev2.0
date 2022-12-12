f09ab962f7447337ad1cd3b9159354fa56ae0945
HenryArpon <henryb.arpon@gmail.com>
2022-12-12 16:21:43 +0800

    First Commit

---
+++ index.js
@@ -0,0 +1,22 @@
+//jshint esversion:6
+require('dotenv').config()
+const express = require('express');
+const ejs = require('ejs'); 
+const bodyParser = require('body-parser'); 
+
+const app = express();
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs'); 
+app.use(bodyParser.urlencoded({extended: true})); 
+
+//GET request 
+
+app.get('/', (req, res) => {
+   res.render('home');
+}); 
+
+
+app.listen(3000, () => {
+     console.log('Server started at port 3000')
+});
